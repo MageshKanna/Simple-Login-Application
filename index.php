20fa8b970434d117925119f6dacb774155e975ad
Mageshkanna <36483110+MageshKanna@users.noreply.github.com>
2022-11-11 21:21:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to Simple Login System!</h1>
+    </header>
+
+    <main>
+        <section class="main-content">
+            <sub>
+                <label for="email">Email Address</label>
+                <input type="email" name="email" placeholder="Enter Your Email here">
+            </sub>
+
+            <sub>
+                <label for="password">Password</label>
+                <input type="password" name="password" placeholder="Enter Your Password here">
+            </sub>
+
+            <sub>
+                <a href="">forgot password?</a>
+                <button type="checkbox">Remember me!</button>
+            </sub>
+
+            <sub>
+                <button type="submit">Login!</button>
+
+                <p>OR</p>
+
+                <a href="">Create your Account Now</a>
+            </sub>
+        </section>
+    </main>
+
+    <footer>
+        <p>© 2022-2023|All Rights Reserved </p>
+    </footer>
+
+</body>
+
+</html>
