4ae8038b199dee8aa5c281aa94b8e7d73801ce72
Mageshkanna <36483110+MageshKanna@users.noreply.github.com>
2023-01-30 23:44:26 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css" />
+    <link rel="stylesheet" href="../nav.css">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class=" d-flex align-items-center justify-content-center" id='varnam' href="#">
+        <img src="../logo.png" id='img' alt="" class='' style='height:50px;'>
+        <label for="img" class='font-weight-bold h5'>Varnam Technologies</label>
+    </div>
+    <div class="cont p-3 d-flex align-items-center justify-content-center">
+        <div class="text-center p-2 d-flex justify-content-center">
+            <form method="post" class="form-group p-2">
+                <h3 class="p-2 font-weight-bold">Please Sign In To Your Account!</h3>
+                <div class="dropdown-divider dd"></div>
+                <div class="status_info">
+                </div>
+                <div class="d-flex flex-column justify-content-right mb-4">
+                    <label for="email" class='text-left font-weight-bold'>
+                        Email
+                    </label>
+                    <input type="email" id="html" name="email" class="form-control mb-2"
+                        placeholder="Email Goes Here...">
+                </div>
+                <div class="d-flex flex-column justify-content-right mb-4">
+                    <label for="password" class='text-left font-weight-bold'>
+                        Password
+                    </label>
+                    <input type="password" id="password" name="password" class="form-control mb-4"
+                        placeholder="Password Goes Here...">
+                </div>
+                <button class="btn btn-primary w-50 m-2" formaction="../Includes/login.inc.php" name="submit"
+                    type="submit">SignIn</button>
+                <h5 class='m-2 font-weight-bold'>OR</h5>
+                <div class="m-2">
+                    <a href="signup.php" class='text-center h5 text-primary font-weight-bold'>
+                        Create your account➜
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
