0dee471ccfe40bacb3953584e6232c00889a0885
Mageshkanna <36483110+MageshKanna@users.noreply.github.com>
2023-01-31 10:48:47 +0530

    Update database.classes.php

--- Classes/database.classes.php
+++ Classes/database.classes.php
@@ -2,21 +2,20 @@
 
 class Database
 {
+  protected function connect(){
+    try {
+      
+      $username = 'root';
+      $pwd = '';
+      
+      $pdo = new PDO('mysql:host=localhost;dbname=test', $username, $pwd);
+      return $pdo;
 
-    protected function connect(){
-          try {
-            
-            $username = 'root';
-            $pwd = '';
-            
-            $pdo = new PDO('mysql:host=localhost;dbname=test', $username, $pwd);
-            return $pdo;
+    } catch (PDOException $e) {
 
-          } catch (PDOException $e) {
-
-            print "ERROR: ". $e-> getMessage() . '<br/>';
-            die();
-            
-          }
+      print "ERROR: ". $e-> getMessage() . '<br/>';
+      die();
+      
     }
+  }
 }
